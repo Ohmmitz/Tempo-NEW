39893329eb3221777e7f0980ca633d1df16e33c9
Ohmmitz <guy.47218@gmail.com>
2023-11-07 10:27:23 +0700

    first commit

---
+++ TEMPO/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TEMPO.Models;
+
+namespace TEMPO.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+
+    public IActionResult index(LoginData abc)
+    {
+        if (ModelState.IsValid)
+        {
+            return View("success", abc);
+        }
+        return View(abc);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+    [HttpPost]
+
+    public IActionResult Register(LoginData abc)
+    {
+        if (ModelState.IsValid)
+        {
+            return View("success", abc);
+        }
+        return View(abc);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ TEMPO/Models/LoginData.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TEMPO.Models
+{
+	public class LoginData
+	{
+        [Key]
+
+        [Required(ErrorMessage = "กรุณาป้อนชื่อ-นามสกุล")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "กรุณาป้อนชื่อผู้ใช้งาน")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "กรุณาป้อนรหัสผ่าน")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "กรุณาป้อนอีเมล์สำหรับติดต่อ")]
+        public string Email { get; set; }
+
+        [Compare("Password", ErrorMessage = "รหัสผ่านไม่ตรงกัน")]
+        [Required(ErrorMessage = "กรุณายืนยันรหัสผ่านอีกครั้ง")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
+
